dabaedbb3d40e9faac741eb495781e853e9c7cd7
Alex12ay <wemadd@outlook.fr>
2024-10-30 13:25:01 +0100

    enlevement js

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,29 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const favoriLink = document.querySelector('.favoris a');
-    if (favoriLink) {
-        favoriLink.addEventListener('click', function(e) {
-            e.preventDefault();
-            const icon = this.querySelector('i');
-            fetch(this.href, {
-                method: 'POST',
-                headers: {
-                    'X-Requested-With': 'XMLHttpRequest'
-                }
-            })
-            .then(response => response.json())
-            .then(data => {
-                if (data.isFavorite) {
-                    icon.className = 'fas fa-star';
-                    icon.style.color = 'black';
-                } else {
-                    icon.className = 'far fa-star';
-                    icon.style.color = '';
-                }
-            })
-            .catch(error => console.error('Erreur:', error));
-        });
-    }
-});
 
        
             
